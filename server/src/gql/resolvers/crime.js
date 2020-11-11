79d53ad98c895e71bfac32c4d6579ea4266c2968
Benjamin Ramsden <benjamin.ramsden@asda.co.uk>
2020-11-11 01:34:39 +0000

    Noticed some postcodes not being populated, writing test to fail on that condition

--- server/src/gql/resolvers/crime.js
+++ server/src/gql/resolvers/crime.js
@@ -1,3 +1,17 @@
+import {getTeams} from "../../net/football-data";
+import {getPostcodes} from "../../net/postcode";
+import {getCrimeData} from "../../net/crime";
+
 export const getStadiums = async function () {
+    // const teams = await getTeams()
+
+    // console.log("teams",teams)
+
+    // const postcodes = await getPostcodes(teams.map(team => team.postcode))
+
+    // console.log("postcodes",postcodes)
+
+    // getCrimeData()
+
     return []
 }
--- server/src/net/football-data.test.js
+++ server/src/net/football-data.test.js
@@ -14,7 +14,26 @@ describe('football-data integration', () => {
         axios.mockReturnValueOnce({
             data: {
                 teams: [
-                    {"id":57,"name":"Arsenal FC","address":"75 Drayton Park London N5 1BU"},
+                    {
+                        id:57,
+                        name:"Arsenal FC",
+                        address:"75 Drayton Park London N5 1BU"
+                    },
+                    {
+                        id: 64,
+                        name: 'Liverpool FC',
+                        address: 'Anfield Road Liverpool L4 OTH',
+                    },
+                    {
+                        id: 73,
+                        name: 'Tottenham Hotspur FC',
+                        address: 'Bill Nicholson Way, 748 High Road London N17 OAP',
+                    },
+                    {
+                        id: 74,
+                        name: 'West Bromwich Albion FC',
+                        address: 'The Hawthorns West Bromwich B71 4LF',
+                    },
                 ]
             }
         })
@@ -27,7 +46,30 @@ describe('football-data integration', () => {
         expect(axios.mock.calls[0][0].url).toEqual('http://api.football-data.org/v2/competitions/2021/teams')
 
         expect(teams).toEqual([
-            {"id":57,"name":"Arsenal FC","address":"75 Drayton Park London N5 1BU","postcode":"N5 1BU"}
+            {
+                id:57,
+                name:"Arsenal FC",
+                address:"75 Drayton Park London N5 1BU",
+                postcode: "N5 1BU"
+            },
+            {
+                id: 64,
+                name: 'Liverpool FC',
+                address: 'Anfield Road Liverpool L4 OTH',
+                postcode:'L4 OTH',
+            },
+            {
+                id: 73,
+                name: 'Tottenham Hotspur FC',
+                address: 'Bill Nicholson Way, 748 High Road London N17 OAP',
+                postcode:'N17 OAP',
+            },
+            {
+                id: 74,
+                name: 'West Bromwich Albion FC',
+                address: 'The Hawthorns West Bromwich B71 4LF',
+                postcode:'B71 4LF',
+            },
         ])
 
     })
